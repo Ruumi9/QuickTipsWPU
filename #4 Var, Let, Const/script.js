786a42b71d0ef9a37eb93115c312e4215f8c4306
Ruumi9 <asepskizo27@gmail.com>
2025-06-25 14:35:37 +0700

    #4 Var, let & const

---
+++ #4 Var, Let, Const/script.js
@@ -0,0 +1,25 @@
+// let  i = 10;
+
+// function test() {
+//   for (var i = 0; i < 10; i++) {
+//     console.log(i);
+//   }
+// }
+// test();
+// console.log(i);
+
+// var x = function () {};
+
+// SIAF
+// (function () {
+//   for (var i = 0; i < 10; i++) {
+//     console.log(i);
+//   }
+// })();
+// console.log(i);
+
+// Kalau pakai let
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+// console.log(i);
